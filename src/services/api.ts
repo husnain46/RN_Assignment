02b7fc7092ea3f423ad70b7f81d7ebfd886d1443
husnain46 <husnainahmed46@gmail.com>
2024-02-16 14:15:30 +0500

    Initial commit

---
+++ src/services/api.ts
@@ -0,0 +1,22 @@
+import {API_URL, AUTH_TOKEN} from '@env';
+import axios from 'axios';
+
+const fetchReminders = async (page: number, pageSize: number) => {
+   try {
+      const response = await axios.get(API_URL, {
+         params: {
+            page,
+            pageSize,
+         },
+         headers: {
+            Authorization: AUTH_TOKEN,
+         },
+      });
+      return response.data;
+   } catch (error) {
+      console.error('Error fetching reminders:', error);
+      throw error;
+   }
+};
+
+export {fetchReminders};
